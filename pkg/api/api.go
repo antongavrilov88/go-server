6133a9de756fd4f552fd6dd65e01dc7b7fb27b60
Anton Gavrilov <antongavrilov88@gmail.com>
2023-06-24 00:08:35 +0800

    feat: add migrations

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -1,7 +1,10 @@
 package api
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
+	"web-server/pkg/db/models"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -14,6 +17,7 @@ func StartAPI(pgdb *pg.DB) *chi.Mux {
 	r.Use(middleware.Logger, middleware.WithValue("DB", pgdb))
 
 	r.Route("/comments", func(r chi.Router) {
+		r.Post("/", createComment)
 		r.Get("/", getComments)
 	})
 
@@ -22,6 +26,82 @@ func StartAPI(pgdb *pg.DB) *chi.Mux {
 	})
 
 	return r
+}
+
+type CreateCommentRequest struct {
+	Comment string `json:"comment"`
+	UserID  int64  `json:"user_id"`
+}
+
+type CommentResponse struct {
+	Success bool            `json:"success"`
+	Error   string          `json:"error"`
+	Comment *models.Comment `json:"comment"`
+}
+
+func createComment(w http.ResponseWriter, r *http.Request) {
+	req := &CreateCommentRequest{}
+	err := json.NewDecoder(r.Body).Decode(req)
+	if err != nil {
+		res := &CommentResponse{
+			Success: false,
+			Error:   err.Error(),
+			Comment: nil,
+		}
+		err = json.NewEncoder(w).Encode(res)
+
+		if err != nil {
+			log.Printf("error sending response %v\n", err)
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	pgdb, ok := r.Context().Value("DB").(*pg.DB)
+
+	if !ok {
+		res := &CommentResponse{
+			Success: false,
+			Error:   "could not get the DB from context",
+			Comment: nil,
+		}
+		err = json.NewEncoder(w).Encode(res)
+		if err != nil {
+			log.Printf("error sending response %v\n", err)
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	comment, err := models.CreateComment(pgdb, &models.Comment{
+		Comment: req.Comment,
+		UserID:  req.UserID,
+	})
+	if err != nil {
+		res := &CommentResponse{
+			Success: false,
+			Error:   err.Error(),
+			Comment: nil,
+		}
+		err = json.NewEncoder(w).Encode(res)
+		if err != nil {
+			log.Printf("error sending response %v\n", err)
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	res := &CommentResponse{
+		Success: true,
+		Error:   "",
+		Comment: comment,
+	}
+	err = json.NewEncoder(w).Encode(res)
+	if err != nil {
+		log.Printf("error encoding after creating comment %v\n", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
 }
 
 func getComments(w http.ResponseWriter, r *http.Request) {
